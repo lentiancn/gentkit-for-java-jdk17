d2d504f2cd11563644f40fd1e0d8dd61d8f0f572
Len <lentiancn@126.com>
2025-11-23 22:30:31 +0800

    docs(utils): add javadoc comments for Base64WithCommonsCodecUtils methods
    - Added detailed JavaDoc comments for all public methods
    - Included multilingual descriptions in Chinese, Traditional Chinese, and English
    - Documented method parameters and return values
    - Added author and version information to class-level JavaDoc
    - Improved code readability and maintainability with comprehensive documentation

--- gentkit-base64/gentkit-base64-with-commons-codec/src/main/java/com/gentkit/base64/utils/Base64WithCommonsCodecUtils.java
+++ gentkit-base64/gentkit-base64-with-commons-codec/src/main/java/com/gentkit/base64/utils/Base64WithCommonsCodecUtils.java
@@ -24,21 +24,61 @@ package com.gentkit.base64.utils;
 import lombok.NoArgsConstructor;
 import org.apache.commons.codec.binary.Base64;
 
+/**
+ * 基于 commons codec 的 Base64 工具。<br>
+ * 基於 commons codec 的 Base64 工具。<br>
+ * Base64 based on commons codec utility.<br>
+ *
+ * @author 田隆 (Len)
+ * @since 2025-11-18 22:29
+ */
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class Base64WithCommonsCodecUtils {
 
+    /**
+     * 解码 Base64 字节数组。<br>
+     * 解碼 Base64 位元組陣列。<br>
+     * Decode Base64 bytes.<br>
+     *
+     * @param base64 Base64 字节数组<br>Base64 位元組陣列<br>Base64 bytes<br>
+     * @return 解码后的字节数组<br>解碼後的位元組陣列<br>Decoded bytes<br>
+     */
     public static byte[] decode(final byte[] base64) {
         return Base64.decodeBase64(base64);
     }
 
+    /**
+     * 解码 Base64 字符串。<br>
+     * 解碼 Base64 字串。<br>
+     * Decode Base64 string.<br>
+     *
+     * @param base64 Base64 字符串<br>Base64 字串<br>Base64 string<br>
+     * @return 解码后的字节数组<br>解碼後的位元組陣列<br>Decoded bytes<br>
+     */
     public static byte[] decodeFromString(final String base64) {
         return Base64.decodeBase64(base64);
     }
 
+    /**
+     * 编码字节数组。<br>
+     * 編碼位元組陣列。<br>
+     * Encode raw bytes.<br>
+     *
+     * @param rawData 原始字节数组<br>原始位元組陣列<br>raw bytes<br>
+     * @return 编码后的字节数组<br>編碼後的位元組陣列<br>Encoded bytes<br>
+     */
     public static byte[] encode(final byte[] rawData) {
         return Base64.encodeBase64(rawData);
     }
 
+    /**
+     * 编码字节数组。<br>
+     * 編碼位元組陣列。<br>
+     * Encode raw bytes.<br>
+     *
+     * @param rawData 原始字节数组<br>原始位元組陣列<br>raw bytes<br>
+     * @return 编码后的 Base64 字符串<br>編碼後的 Base64 字串<br>Encoded Base64 string<br>
+     */
     public static String encodeToString(final byte[] rawData) {
         return Base64.encodeBase64String(rawData);
     }
