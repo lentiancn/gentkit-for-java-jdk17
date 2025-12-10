ec0e19e2f0b127f73e7ffd798c2cc1708e981402
Len <lentiancn@126.com>
2025-12-10 14:17:53 +0800

    feat(flux-web): 添加全局异常处理器MIT许可证声明
    - 在GlobalExceptionHandler类中增加MIT许可证头部注释
    - 优化全局异常处理逻辑，直接使用异常中的状态码和消息
    - 统一异常日志记录方式，使用getClass()获取当前类的日志记录器
    - 在网关模块pom.xml中添加对gentkit-component-flux-web组件的依赖引用

--- gentkit-server/gentkit-server-with-bca/gentkit-component/gentkit-component-flux-web/src/main/java/com/gentkit/server/component/flux/web/handler/GlobalExceptionHandler.java
+++ gentkit-server/gentkit-server-with-bca/gentkit-component/gentkit-component-flux-web/src/main/java/com/gentkit/server/component/flux/web/handler/GlobalExceptionHandler.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 田隆 (Len)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.gentkit.server.component.flux.web.handler;
 
 import com.gentkit.common.result.model.FailureResultStatus;
@@ -21,20 +44,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(GlobalException.class)
     public ResponseEntity<Result<?>> handleGlobalException(GlobalException e) {
-        FailureResultStatus status = FailureResultStatus.SYSTEM_ERROR;
-        Object data = null;
-
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
-                .body(Result.failure(status, e.getMessage(), data));
+                .body(Result.failure(e.getStatus(), e.getMessage(), null));
     }
 
     @ExceptionHandler(UnsupportedMediaTypeStatusException.class)
     public ResponseEntity<Result<?>> handleUnsupportedMediaTypeStatusException(UnsupportedMediaTypeStatusException e) {
+        LoggerUtils.getLogger(getClass()).error(e);
+
         FailureResultStatus status = FailureResultStatus.UNSUPPORTED_MEDIA_TYPE;
         String message = status.getMessage();
 
-        return handleGlobalException(new GlobalException(message, e));
+        return handleGlobalException(new GlobalException(status, message, e));
     }
 
     @ExceptionHandler(Throwable.class)
@@ -43,7 +65,7 @@ public class GlobalExceptionHandler {
             return handleGlobalException(e1);
         }
 
-        LoggerUtils.getLogger(GlobalExceptionHandler.class).error(e);
+        LoggerUtils.getLogger(getClass()).error(e);
 
         FailureResultStatus status = FailureResultStatus.SYSTEM_ERROR;
         String message = status.getMessage();
