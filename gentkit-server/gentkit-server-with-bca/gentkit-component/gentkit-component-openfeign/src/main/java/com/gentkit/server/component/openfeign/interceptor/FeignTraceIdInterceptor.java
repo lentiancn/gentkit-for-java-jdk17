0a546b7521e0cf26a24633291769887c17378ae5
Len <lentiancn@126.com>
2025-12-10 17:50:16 +0800

    feat(openfeign): 新增OpenFeign组件及其自动配置
    - 添加FeignTraceIdInterceptor拦截器以传递traceId
    - 创建OpenFeignAutoConfiguration自动配置类
    - 注册FeignTraceIdInterceptor Bean
    - 在spring.factories中启用自动配置
    - 引入spring-cloud-starter-openfeign依赖
    - 添加对gentkit-common和reactor-core的依赖
    - 将openfeign模块加入component父模块
    - 在gateway-flux-server中引入openfeign组件依赖

---
+++ gentkit-server/gentkit-server-with-bca/gentkit-component/gentkit-component-openfeign/src/main/java/com/gentkit/server/component/openfeign/interceptor/FeignTraceIdInterceptor.java
@@ -0,0 +1,40 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 田隆 (Len)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.gentkit.server.component.openfeign.interceptor;
+
+import com.gentkit.common.traceid.TraceIdContext;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+public class FeignTraceIdInterceptor implements RequestInterceptor {
+
+    public static final String X_TRACE_ID_HEADER = "X-Trace-Id";
+
+    @Override
+    public void apply(RequestTemplate template) {
+        String traceId = TraceIdContext.get();
+
+        template.header(X_TRACE_ID_HEADER, traceId);
+    }
+}
